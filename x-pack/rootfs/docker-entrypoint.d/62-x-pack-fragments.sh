aaeafeb3e7b661ddeafab71d362aecb8899fb20b
Petr Řehoř <rx@rx.cz>
2017-10-01 23:38:57 +0200

    Add --stdin to elasticsearch-keystore add command

--- x-pack/rootfs/docker-entrypoint.d/62-x-pack-fragments.sh
+++ x-pack/rootfs/docker-entrypoint.d/62-x-pack-fragments.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+!/bin/bash -e
 
 ### ELASTICSEARCH_YML ##########################################################
 
@@ -8,12 +8,12 @@ if [ ! -e ${ES_PATH_CONF}/elasticsearch.x-pack.default-tls-settings.yml ]; then
     echo "xpack.ssl.supported_protocols: TLSv1.2"
     if [ -n "${JAVA_TRUSTSTORE_FILE}" -a -e "${JAVA_TRUSTSTORE_FILE}" ]; then
       echo "xpack.ssl.truststore.path: ${JAVA_TRUSTSTORE_FILE}"
-      # TODO: Elasticsearch 6.0.0-beta2 crashes with keystore passwords in Elasticsearch keystore
+      # TODO: Elasticsearch 6.0.0-beta2 crashes with truststore password in Elasticsearch keystore
       echo "xpack.ssl.truststore.password: ${JAVA_TRUSTSTORE_PWD}"
     fi
     if [ -n "${JAVA_KEYSTORE_FILE}" -a -e "${JAVA_KEYSTORE_FILE}" ]; then
       echo "xpack.ssl.keystore.path: ${JAVA_KEYSTORE_FILE}"
-      # TODO: Elasticsearch 6.0.0-beta2 crashes with keystore passwords in Elasticsearch keystore
+      # TODO: Elasticsearch 6.0.0-beta2 crashes with keystore password in Elasticsearch keystore
       echo "xpack.ssl.keystore.password: ${JAVA_KEYSTORE_PWD}"
     fi
   ) > ${ES_PATH_CONF}/elasticsearch.x-pack.default-tls-settings.yml
@@ -31,17 +31,17 @@ if [ ! -e "${ES_PATH_CONF}/elasticsearch.keystore" ]; then
   elasticsearch-keystore create
   for XPACK_SECRET_FILE in $(ls /run/secrets/*.secret 2> /dev/null); do
     XPACK_SECRET="$(basename ${XPACK_SECRET_FILE} | sed -E "s/.secret$")"
-    cat ${XPACK_SECRET_FILE} | elasticsearch-keystore add "${XPACK_SECRET}"
+    cat ${XPACK_SECRET_FILE} | elasticsearch-keystore add --stdin "${XPACK_SECRET}"
   done
-  # TODO: Elasticsearch 6.0.0-beta2 crashes with keystore passwords in Elasticsearch keystore
+  # TODO: Elasticsearch 6.0.0-beta2 crashes with passwords in Elasticsearch keystore
   # if [ -n "${JAVA_TRUSTSTORE_PWD}" ]; then
-  #   echo "${JAVA_TRUSTSTORE_PWD}" | elasticsearch-keystore add "xpack.ssl.truststore.password"
+  #   echo "${JAVA_TRUSTSTORE_PWD}" | elasticsearch-keystore add --stdin "xpack.ssl.truststore.password"
   # fi
   # if [ -n "${JAVA_KEYSTORE_PWD}" ]; then
-  #   echo "${JAVA_KEYSTORE_PWD}" | elasticsearch-keystore add "xpack.ssl.keystore.password"
+  #   echo "${JAVA_KEYSTORE_PWD}" | elasticsearch-keystore add --stdin "xpack.ssl.keystore.password"
   # fi
   if [ -n "${XPACK_BOOTSTRAP_PASSWORD}" ]; then
-    echo "${XPACK_BOOTSTRAP_PASSWORD}" | elasticsearch-keystore add "bootstrap.password"
+    echo "${XPACK_BOOTSTRAP_PASSWORD}" | elasticsearch-keystore add --stdin "bootstrap.password"
   fi
   if [ -n "${DOCKER_ENTRYPOINT_DEBUG}" ]; then
     elasticsearch-keystore list
